2ae72d629bda100dd71ebd023315b968cdbcf7f6
MrK1ngman <ilya.razin@mail.ru>
2025-01-26 22:27:25 +0700

    add img

--- taskmanager/src/js/app.js
+++ taskmanager/src/js/app.js
@@ -4,3 +4,4 @@ const taskManager = new TaskManager();
 
 document.addEventListener('DOMContentLoaded', taskManager.render());
 
+document.querySelector('.manager').addEventListener('click', (e) => taskManager.control(e))
