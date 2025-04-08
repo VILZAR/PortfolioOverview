84e4b6e1798ed25109731a77bfbea9d190dffd4f
Ilya <97036370+VILZAR@users.noreply.github.com>
2025-04-09 00:03:32 +0700

    Delete taskmanager directory

--- taskmanager/src/js/app.js
+++
@@ -1,7 +0,0 @@
-import TaskManager from "./TaskManager";
-
-const taskManager = new TaskManager();
-
-document.addEventListener('DOMContentLoaded', taskManager.render());
-
-document.querySelector('.manager').addEventListener('click', (e) => taskManager.control(e))
